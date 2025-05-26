de5c6ef7ee9e56b697c2344b834129d9a7a6ae8c
minhtuan23 <hoangminh2306602@gmail.com>
2025-05-26 15:05:36 +0700

    check

--- page/pageAction/login.ts
+++ page/pageAction/login.ts
@@ -3,6 +3,7 @@ import { loginPage } from '../pageLocator/login';
 import { test } from '@playwright/test';
 
 export async function login_page(page: Page, username: string, password: string) {
+    // chec
     await page.waitForSelector(loginPage.passwordInput);
     await page.fill(loginPage.usernameInput, username);
     await page.fill(loginPage.passwordInput, password);
